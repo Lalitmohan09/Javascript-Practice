c546688d7cdec3383128ebde43e4075e956351a0
Lalit Mohan Singh <128134086+Lalitmohan09@users.noreply.github.com>
2023-12-14 00:36:09 +0530

    Update right_angle_triangle.js

--- Patterns/right_angle_triangle.js
+++ Patterns/right_angle_triangle.js
@@ -2,13 +2,12 @@ let num = 5;
 let string = "";
 
 function print(num) {
-  for (let i = 0; i < num; i++) {
-    for (let j = 0; j <= i; j++) {
+  for (let i = 1; i <= num; i++) {
+    for (let j = 0; j < i; j++) {
       string += "*";
-      console.log(string);
-      if(string.length===num) return
     }  
-   
+    string += "\n"
   }
+  return string;
 }
-print(num);
+console.log(print(num));
