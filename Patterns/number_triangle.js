1347186344ab101868aa17eb88961d6a0ab409dc
Lalit Mohan Singh <128134086+Lalitmohan09@users.noreply.github.com>
2023-12-14 02:35:01 +0530

    Added some Comments

--- Patterns/number_triangle.js
+++ Patterns/number_triangle.js
@@ -1,7 +1,14 @@
 let num = 5;
 let string = "";
 let n = 0;
-
+/*
+Printing pattern:-
+1
+12
+123
+1234
+12345
+*/
 // function print(num) {
 //   for (let i = 0; i < num; i++) {
 //     for (let j = 0; j <= i; j++) {
@@ -15,9 +22,17 @@ let n = 0;
 // }
 // console.log(print(num));
 
+/*
+Printing Patterns:-
+1
+22
+333
+4444
+55555
+*/
 // let string2 = ""
-// n = 1
 // function print2(num) {
+//     n = 1
 //     for(let i = 1; i<=num; i++){
 //         for(let j = 0; j<i; j++){
 //             string2 +=n;
@@ -27,9 +42,18 @@ let n = 0;
 //     }
 //     console.log(string2)
 // }
-// print2(num)
+// print2(num);
 
+/*
+Printing pattern:-
+12345
+1234
+123
+12
+1
+*/
 function opposite(num) {
+    n=0
     for(let i= 0; i<num; i++){
         for(let j = num; j>i; j--){
             n++
